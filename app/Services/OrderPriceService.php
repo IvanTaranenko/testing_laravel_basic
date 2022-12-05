c2423b13fef200f214c8c6c2d7d24b4782827bdd
IvanTaranenko <vanokalipsi@gmail.com>
2022-12-05 18:34:01 +0200

    first commit

---
+++ app/Services/OrderPriceService.php
@@ -0,0 +1,13 @@
+<?php
+
+
+ namespace App\Services;
+
+
+ class OrderPriceService
+ {
+     public function calculatePrice($productId, $quantity, $tax = 0.0)
+     {
+         // Some kind of calculation logic
+     }
+ }
---
+++ tests/Unit/OrderPriceTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Product;
+use App\Services\OrderPriceService;
+use PHPUnit\Framework\TestCase;
+
+class OrderPriceTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+
+
+
+
+}
